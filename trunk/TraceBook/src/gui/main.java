699a7791231bce9edd9380ad98d41ee82a8171f0
SakirSoft <SakirSoft@5eff381b-bd8e-e3be-69db-190002558077>
2011-02-28 14:47:29 +0000

    -trace book ;)

---
+++ trunk/TraceBook/src/gui/main.java
@@ -0,0 +1,15 @@
+package gui;
+
+import Trace.Book.R;
+import Trace.Book.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
