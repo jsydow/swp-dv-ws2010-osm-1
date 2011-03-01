5fe83616a889b175c10eec5ebd6d54dacd69a2fa
greentraxas@googlemail.com <greentraxas@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-01 13:25:22 +0000

    GUI: - Tab Map - Tab New - Tab Edit

--- trunk/TraceBook/src/gui/LoadTrackActivity.java
+++ trunk/TraceBook/src/gui/LoadTrackActivity.java
@@ -1,7 +1,13 @@
 package gui;
 
+import Trace.Book.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LoadTrackActivity extends Activity {
 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loadtrackactivity);
+    }
 }
