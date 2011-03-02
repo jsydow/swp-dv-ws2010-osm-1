8162a627b8e5ba90bc157f2cc9a1dde87e428fe9
SakirSoft <SakirSoft@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-02 15:17:14 +0000

    -service is communicating again ;)

---
+++ trunk/TraceBook/src/core/logger/LoggerServiceConnection.java
@@ -0,0 +1,30 @@
+package core.logger;
+
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
+/**
+ * Stub for RPC communication with the logger Service  
+ * @author benpicco
+ *
+ */
+public class LoggerServiceConnection implements ServiceConnection {
+	
+	ILoggerService loggerService = null;
+	
+	
+	public void onServiceConnected(ComponentName className, IBinder boundService) {
+		loggerService = ILoggerService.Stub.asInterface((IBinder) boundService);
+		//Log.d(LOG_TAG, "onServiceConnected");
+	}
+
+	public void onServiceDisconnected(ComponentName className) {
+		loggerService = null;
+		//Log.d(LOG_TAG, "onServiceDisconnected");
+	}
+	
+	public ILoggerService getLoggerService() {
+		return loggerService;
+	}
+}
