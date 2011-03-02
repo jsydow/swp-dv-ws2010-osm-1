acef34b6f925e215b7d045d92514515855bcbe8a
anubis@cstilch.com <anubis@cstilch.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-02 09:28:31 +0000

    Logparameter class for start logging remote call

---
+++ trunk/TraceBook/src/core/data/LogParameter.java
@@ -0,0 +1,12 @@
+package core.data;
+
+public class LogParameter {
+	public int ID=-1;
+	public int delta_distance=10;
+	public int delta_time=2000;
+	public LogParameter(int ID,int delta_distance,int delta_time){
+		this.ID=ID;
+		this.delta_distance=delta_distance;
+		this.delta_time=delta_time;
+	}
+}
