3626ca2eef44b723be776dff15313b44baeaf2c5
SakirSoft <SakirSoft@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-02 14:08:51 +0000

    -put babby sees the world hopefully

--- TraceBook/src/gui/main.java
+++ TraceBook/src/gui/main.java
@@ -1,9 +1,12 @@
 package gui;
 
+import core.data.LogParameter;
+import core.logger.ServiceConnector;
 import Trace.Book.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -13,11 +16,22 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ServiceConnector.initService(this);
     }
     
-    public void newTrack(View view){
+    public void newTrack(View view) {
     	Intent intent = new Intent(this, NewTrackActivity.class);
     	startActivity(intent);
+    	
+    	LogParameter param = new LogParameter();
+    	
+    	try {
+			ServiceConnector.getLoggerService().addTrack(param, false);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     public void loadTrack(View view){
