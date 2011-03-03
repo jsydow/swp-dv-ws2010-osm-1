79410a780638c0d03ca97f7e6daf704f40015f8c
androidosm.n <androidosm.n@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-03 10:34:37 +0000

    added javadoc

--- TraceBook/src/gui/EditPointActivity.java
+++ TraceBook/src/gui/EditPointActivity.java
@@ -4,12 +4,20 @@ import Trace.Book.R;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class EditPointActivity extends Activity{
-	
+/**
+ * The Class EditPointActivity.
+ */
+public class EditPointActivity extends Activity {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.loadtrackactivity);
-    }
-	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loadtrackactivity);
+	}
+
 }
--- TraceBook/src/gui/LoadTrackActivity.java
+++ TraceBook/src/gui/LoadTrackActivity.java
@@ -4,11 +4,19 @@ import Trace.Book.R;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * The Class LoadTrackActivity.
+ */
 public class LoadTrackActivity extends Activity {
-	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.loadtrackactivity);
-    }
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loadtrackactivity);
+	}
 }
