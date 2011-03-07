808798866d060bbf97b642a6cce882df3a755b89
greentraxas@googlemail.com <greentraxas@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-07 14:07:13 +0000

    Update: - Add more MetaTags to one POI - Show MetaInformation by adding some new one

--- trunk/TraceBook/src/gui/AddPointActivity.java
+++ trunk/TraceBook/src/gui/AddPointActivity.java
@@ -1,50 +1,96 @@
 package gui;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import core.logger.ServiceConnector;
 import Trace.Book.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import core.data.DataNode;
 import core.data.DataStorage;
+import core.logger.ServiceConnector;
 
 public class AddPointActivity extends Activity {
 
-	TextView textView;
+	ListView listView;
 	DataNode node;
+	String[] metaInformation;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.addpointactivity);
 
-		//Init ServiceConnector
-        ServiceConnector.startService(this);
-        ServiceConnector.initService();
-        
-        listNodeInformation();
+		// Init ServiceConnector
+		ServiceConnector.startService(this);
+		ServiceConnector.initService();
+
+		getNodeInformation();
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, getNodeInformation());
+		listNodeInformation(adapter);
+
 	}
-	
-	public void listNodeInformation(){
-		textView = (TextView) findViewById(R.id.nodeTags);
-		List<DataNode> nodeList = DataStorage.getInstance().getCurrentTrack().getNodes();
-		if( nodeList.size() != 0 ){
-   			node = nodeList.get(nodeList.size()-1);
-			textView.setText("NodeID: " + node.get_id());
+
+	private String[] getNodeInformation() {
+
+		String meta = new String();
+		List<DataNode> nodeList = DataStorage.getInstance().getCurrentTrack()
+				.getNodes();
+		int i=0;
+		if (nodeList.size() != 0) {
+			node = nodeList.get(nodeList.size() - 1);
+			Map<String, String> tagMap = node.getTags();
+			metaInformation = new String[tagMap.size()];
+
+			Iterator<Entry<String, String>> iterator = tagMap.entrySet()
+					.iterator();
+
+			while (iterator.hasNext()) {
+				Map.Entry<String, String> pairs = iterator.next();
+				meta = pairs.getKey() + " - " + pairs.getValue();
+				metaInformation[i] = meta;
+				i++;
+			}
+		} else{
+			metaInformation = new String[1];
+			metaInformation[0] = "Keine Informationen vorhanden";
+			Toast.makeText(this, "No Node tracked yet", Toast.LENGTH_SHORT)
+					.show();
 		}
-   		else
-   			Toast.makeText(this, "No Node tracked yet", Toast.LENGTH_SHORT).show();
+		return metaInformation;
 	}
-	
-	public void addPointMetaBtn(View view){
-		final Intent intent = new Intent(this,AddPointMetaActivity.class);
+
+	public void listNodeInformation(ArrayAdapter<String> adapter) {
+		listView = (ListView) findViewById(R.id.meta_lvw);
+		listView.setAdapter(adapter);
+		listView.setTextFilterEnabled(true);
+		
+		//Get selected item and send toast
+		listView.setOnItemClickListener(new OnItemClickListener() {
+		    public void onItemClick(AdapterView<?> parent, View view,
+		        int position, long id) {
+		      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+		          Toast.LENGTH_SHORT).show();
+		    }
+		  });
+	}
+
+	public void addPointMetaBtn(View view) {
+		final Intent intent = new Intent(this, AddPointMetaActivity.class);
 		startActivity(intent);
 	}
-	
+
 	public void cancelBtn(View view) {
 		final Intent intent = new Intent(this, NewTrackActivity.class);
 		startActivity(intent);
