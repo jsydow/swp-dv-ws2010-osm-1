20c327bc2e32f9e6c7148d18adbaad5a07631d05
benpicco <benpicco@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-07 10:27:09 +0000

    do not crash if map file does not exist

--- TraceBook/src/gui/MapsForgeActivity.java
+++ TraceBook/src/gui/MapsForgeActivity.java
@@ -1,6 +1,10 @@
 package gui;
 
+import java.io.File;
+
 import android.os.Bundle;
+import android.widget.Toast;
+
 import org.mapsforge.android.maps.MapActivity;
 import org.mapsforge.android.maps.MapView;
 
@@ -8,10 +12,18 @@ public class MapsForgeActivity extends MapActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        File file = new File("/sdcard/", "default.map");
+        if(!file.exists()) {
+		      Toast.makeText(getApplicationContext(), "Unable to open "+file.getAbsolutePath()+"!", Toast.LENGTH_LONG).show();
+		      return;
+        }
+        
         MapView mapView = new MapView(this);
         mapView.setClickable(true);
         mapView.setBuiltInZoomControls(true);
-        mapView.setMapFile("/sdcard/default.map");
+        mapView.setMapFile(file.getAbsolutePath());
+        
         setContentView(mapView);
     }
 }
