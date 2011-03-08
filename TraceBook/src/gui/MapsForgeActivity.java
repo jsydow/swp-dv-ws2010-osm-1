c2f9c149db91e49822c1d8d6cc1e9c8d673eb852
benpicco <benpicco@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-08 10:19:00 +0000

    *should* mark the current position now

--- TraceBook/src/gui/MapsForgeActivity.java
+++ TraceBook/src/gui/MapsForgeActivity.java
@@ -2,27 +2,43 @@ package gui;
 
 import java.io.File;
 
+import org.mapsforge.android.maps.ArrayItemizedOverlay;
 import org.mapsforge.android.maps.GeoPoint;
 import org.mapsforge.android.maps.MapActivity;
 import org.mapsforge.android.maps.MapController;
 import org.mapsforge.android.maps.MapView;
+import org.mapsforge.android.maps.OverlayItem;
 import org.mapsforge.android.maps.RouteOverlay;
 
-import core.data.DataStorage;
-import core.logger.WaypointLogService;
-
+import Trace.Book.R;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
+import core.data.DataNode;
+import core.data.DataPointsList;
+import core.data.DataStorage;
+import core.data.DataTrack;
+import core.logger.WaypointLogService;
 
+/**
+ * This class implements a MapsForge Map activity and draws ways and nodes
+ * as overlays on it.
+ * In the future it will also be possible to modify said nodes and ways. 
+ * @author benpicco
+ *
+ */
 public class MapsForgeActivity extends MapActivity {
+	final static String LOG_TAG = "MapsForgeActivity";
+	
 	MapController mapController;
-	RouteOverlay rOverlay;
+	RouteOverlay routesOverlay;
+	ArrayItemizedOverlay pointsOverlay;
 	BroadcastReceiver gpsReceiver;
 	
 	@Override
@@ -41,7 +57,12 @@ public class MapsForgeActivity extends MapActivity {
 		mapView.setClickable(true);
 		mapView.setBuiltInZoomControls(true);
 		mapView.setMapFile(file.getAbsolutePath());
-
+		
+		if(android.os.Build.MODEL.equals("sdk")) {
+			Log.d(LOG_TAG, "Running in emulator - disabling SD cache");
+			mapView.setMemoryCardCacheSize(0);
+		}
+		
 		setContentView(mapView);
 		
 		mapController = mapView.getController();
@@ -51,9 +72,13 @@ public class MapsForgeActivity extends MapActivity {
 		Paint filling = new Paint();
 		filling.setARGB(100, 255, 0, 0);
 		
-		rOverlay = new RouteOverlay(filling, outline);
+		final Drawable defaultMarker = getResources().getDrawable(R.drawable.marker_red);
 		
-		mapView.getOverlays().add(rOverlay);
+		routesOverlay = new RouteOverlay(filling, outline);
+		pointsOverlay = new ArrayItemizedOverlay(defaultMarker, this);
+		
+		mapView.getOverlays().add(routesOverlay);
+		mapView.getOverlays().add(pointsOverlay);
 		
 		gpsReceiver = new GPSReceiver();
 	}
@@ -61,7 +86,7 @@ public class MapsForgeActivity extends MapActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		registerReceiver(gpsReceiver, new IntentFilter(WaypointLogService.UPDTAE_GPS));
+		registerReceiver(gpsReceiver, new IntentFilter(WaypointLogService.UPDTAE_GPS_POS));
 	}
 	
 	@Override
@@ -70,25 +95,81 @@ public class MapsForgeActivity extends MapActivity {
 		unregisterReceiver(gpsReceiver);
 	}
 	
+	/**
+	 * This class receives Broadcast Messages from the WaypointLogService in order
+	 * to update the current location and overlays if position or overlay data has
+	 * changed.
+	 * @author benpicco
+	 *
+	 */
 	private class GPSReceiver extends BroadcastReceiver {
-		public GPSReceiver() {
-			// TODO Auto-generated constructor stub
+		OverlayItem current_pos;
+
+		private OverlayItem getOverlayItem(GeoPoint pos, int marker) {
+			OverlayItem oi = new OverlayItem(pos, "foo", "bar");
+			oi.setMarker(getResources().getDrawable(marker));
+			
+			return oi;
 		}
+		
+		public GPSReceiver() { /* nothing to do here */ }
 
 		@Override
 		public void onReceive(Context ctx, Intent intend) {
 			
-			if(intend.getAction().equals(WaypointLogService.UPDTAE_GPS)) {
-				double lng = intend.getExtras().getDouble("long");
-				double lat = intend.getExtras().getDouble("lat");
+			// Receive current location and do something with it
+			if(intend.getAction().equals(WaypointLogService.UPDTAE_GPS_POS)) {
+				final double lng = intend.getExtras().getDouble("long");
+				final double lat = intend.getExtras().getDouble("lat");
 				
-				mapController.setCenter(new GeoPoint(lat, lng));
+				final GeoPoint pos = new GeoPoint(lat, lng);
 				
-				Log.d("GPS_REC", "Location update received (" + lng +  ", " + lat +")");
-			} else if(intend.getAction().equals(WaypointLogService.UPDTAE_WAY)) {
-				int id = intend.getExtras().getInt("way_id");
-				rOverlay.setRouteData(DataStorage.getInstance().getCurrentTrack().getWays().get(id).toGeoPoitArray());
+				mapController.setCenter(pos);
+				
+				Log.d(LOG_TAG, "Location update received (" + lng +  ", " + lat +")");
+				
+				pointsOverlay.removeOverlay(current_pos);
+				pointsOverlay.addOverlay(getOverlayItem(pos, R.drawable.marker_green));
+				
+				
+			// Receive an update of a way or a node and update the overlay accordingly 
+			} else if(intend.getAction().equals(WaypointLogService.UPDTAE_OBJECT)) {
+				if(currentTrack() == null) {
+					Log.e(LOG_TAG, "Received UPDATE_OBJECT with no current track present.");
+					return;
+				}
+				
+				int way_id = intend.getExtras().getInt("way_id");
+				int point_id = intend.getExtras().getInt("point_id");
+				if(way_id > 0) {
+					Log.d(LOG_TAG, "Received way update, id="+way_id);
+					DataPointsList way = currentTrack().getWays().get(way_id);
+					if(way == null)
+						Log.e(LOG_TAG, "Way with ID " + way_id + " does not exist.");
+					else {
+						routesOverlay.setRouteData(way.toGeoPoitArray());
+					}
+					
+				} else if(point_id > 0) {
+					Log.d("LOG_TAG", "Received node update, id="+point_id);
+					DataNode point = currentTrack().getNodes().get(point_id);
+					if(point == null)
+						Log.e(LOG_TAG, "Node with ID " + point_id + " does not exist.");
+					else {
+						pointsOverlay.addOverlay(
+								new OverlayItem(new GeoPoint(point.getLat(), point.getLon()),
+								point.get_id() + "", ""));
+					}
+				}
 			}
 		}
+	}
+	
+	/**
+	 * gets the current DataTrack for convenience
+	 * @return
+	 */
+	static DataTrack currentTrack() { 
+		return DataStorage.getInstance().getCurrentTrack();
 	}
 }
