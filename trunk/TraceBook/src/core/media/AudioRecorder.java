b26c35e7e5e9bca517907782d054820dc40b836c
Skudo84@gmail.com <Skudo84@gmail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-09 18:28:04 +0000

    Creating a separate package for media recording.

---
+++ trunk/TraceBook/src/core/media/AudioRecorder.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package core.media;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.media.MediaRecorder;
+
+/**
+ * @author Huy Dinh
+ * 
+ */
+public class AudioRecorder extends Recorder {
+    /**
+     * 
+     */
+    protected MediaRecorder recorder = new MediaRecorder();
+
+    /**
+     * 
+     */
+    public AudioRecorder() {
+        super();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see core.media.Recorder#getNewFilename()
+     */
+    @Override
+    protected String getNewFilename() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String newFilename = sdf.format(new Date());
+
+        newFilename = "audio_" + newFilename + ".m4a";
+
+        return newFilename;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see core.media.Recorder#init()
+     */
+    @Override
+    public void init() {
+        // Nothing to be done here, really...
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see core.media.Recorder#start()
+     */
+    @Override
+    public String start() {
+        if (isRecording) {
+            return null;
+        }
+
+        filename = getNewFilename();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        // Possible output formats are 3gpp and MPEG4, e. g.
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        recorder.setOutputFile(getPath());
+
+        try {
+            recorder.prepare();
+            recorder.start();
+            isRecording = true;
+
+            return filename;
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see core.media.Recorder#stop(core.data.DataMediaHolder)
+     */
+    @Override
+    public void stop() {
+        if (isRecording) {
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+
+            isRecording = false;
+        }
+    }
+}
