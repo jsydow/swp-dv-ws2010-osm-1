37928b3a1fce01138a40287f0a3f59d30d1cde00
SakirSoft <SakirSoft@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-09 11:34:11 +0000

    -fix

--- TraceBook/src/gui/AddPointMetaActivity.java
+++ TraceBook/src/gui/AddPointMetaActivity.java
@@ -20,20 +20,19 @@ import core.data.DataMapObject;
 import core.data.DataStorage;
 
 /**
- * @author greentrax
- * In this Activty you can choose your Tags via an autocomplete feature.
- * Tags and values are grouped together. So the autocomplete feature for values 
- * are depended  by their key values. For example when you choose "highway" only 
- * highway related values will be given as a list for autocomplete.
- *
+ * @author greentrax In this Activty you can choose your Tags via an
+ *         autocomplete feature. Tags and values are grouped together. So the
+ *         autocomplete feature for values are depended by their key values. For
+ *         example when you choose "highway" only highway related values will be
+ *         given as a list for autocomplete.
+ * 
  */
 public class AddPointMetaActivity extends Activity {
 
     /**
-     * @author greentraxas
-     * A simple enum class for tags
+     * @author greentraxas A simple enum class for tags
      */
-    enum Tags{
+    enum Tags {
         /**
          * 
          */
@@ -46,196 +45,196 @@ public class AddPointMetaActivity extends Activity {
          * 
          */
         USEFUL
-        
+
     }
-	
-	
-	/**
-	 * Reference to the current DataMapObject in use
-	 */
-	DataMapObject node;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.addpointmetaactivity);
 
-		
-		final AutoCompleteTextView autoComplVal = (AutoCompleteTextView) findViewById(R.id.autoComplete_Value);
-		final AutoCompleteTextView autoComplCat = (AutoCompleteTextView) findViewById(R.id.autoComplete_Cat);
+    /**
+     * Reference to the current DataMapObject in use
+     */
+    DataMapObject node;
 
-		final Bundle extras = getIntent().getExtras();
-		
-		if (extras != null) {
-			int nodeId = extras.getInt("DataNodeId");
-			node = DataStorage.getInstance().getCurrentTrack().getDataMapObjectById(nodeId);
-			
-			if( extras.containsKey("DataNodeKey") ) {
-				String keyValue = extras.getString("DataNodeKey");
-				String[] cat = keyValue.split(" - ");
-				autoComplCat.setText(cat[0]);
-				autoComplVal.setText(cat[1]);
-			}
-		}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addpointmetaactivity);
 
-		ArrayAdapter<String> firstGroupAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_dropdown_item_1line, getCategoryTags());
-		autoComplCat.setAdapter(firstGroupAdapter);
+        final AutoCompleteTextView autoComplVal = (AutoCompleteTextView) findViewById(R.id.autoComplete_Value);
+        final AutoCompleteTextView autoComplCat = (AutoCompleteTextView) findViewById(R.id.autoComplete_Cat);
 
-		/**
-		 * If the focus is at the AutoCompleteTextView autoComplVal we call the
-		 * method getValues to generate the AutoComplete String[]
-		 */
-		autoComplVal.setOnFocusChangeListener(new OnFocusChangeListener() {
+        final Bundle extras = getIntent().getExtras();
 
-			public void onFocusChange(View v, boolean hasFocus) {
-				if (hasFocus) {
-					String cat = autoComplCat.getText().toString();
+        if (extras != null) {
+            int nodeId = extras.getInt("DataNodeId");
+            node = DataStorage.getInstance().getCurrentTrack()
+                    .getDataMapObjectById(nodeId);
 
-					// autoComplVal.setText(cat.toCharArray(),0,cat.length());
-					ArrayAdapter<String> valueTagAdapter = new ArrayAdapter<String>(
-							v.getContext(),
-							android.R.layout.simple_dropdown_item_1line,
-							getValues(cat));
-					autoComplVal.setAdapter(valueTagAdapter);
+            if (extras.containsKey("DataNodeKey")) {
+                String keyValue = extras.getString("DataNodeKey");
+                String[] cat = keyValue.split(" - ");
+                autoComplCat.setText(cat[0]);
+                autoComplVal.setText(cat[1]);
+            }
+        }
 
-				}
-			}
-		});
-	}
+        ArrayAdapter<String> firstGroupAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, getCategoryTags());
+        autoComplCat.setAdapter(firstGroupAdapter);
 
-	/**
-	 * Give all the tag category's from the Tag-XML.
-	 * 
-	 * @return a string array containing the category's 
-	 */
-	public String[] getCategoryTags() {
-		// Testarray
-		String[] firstGroupTags = parseTags(Tags.KEY, "");
-		return firstGroupTags;
-	}
+        /**
+         * If the focus is at the AutoCompleteTextView autoComplVal we call the
+         * method getValues to generate the AutoComplete String[]
+         */
+        autoComplVal.setOnFocusChangeListener(new OnFocusChangeListener() {
 
-	/**
-	 * Generate the appendant values for the category tag.
-	 * 
-	 * @param category
-	 * @return return a string array with values for the given category
-	 */
-	public String[] getValues(String category) {
-		// return the value tags for the selected category tag
-		String[] valueTags = parseTags(Tags.VALUE, category);
-		return valueTags;
-	}
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    String cat = autoComplCat.getText().toString();
 
-	/**
-	 * Save the MetaData to the Node-Meta and go back to the information
-	 * activity.
-	 * 
-	 * @param view
-	 */
-	public void saveBtn(View view) {
+                    // autoComplVal.setText(cat.toCharArray(),0,cat.length());
+                    ArrayAdapter<String> valueTagAdapter = new ArrayAdapter<String>(
+                            v.getContext(),
+                            android.R.layout.simple_dropdown_item_1line,
+                            getValues(cat));
+                    autoComplVal.setAdapter(valueTagAdapter);
 
-		final AutoCompleteTextView autoComplVal = (AutoCompleteTextView) findViewById(R.id.autoComplete_Value);
-		final AutoCompleteTextView autoComplCat = (AutoCompleteTextView) findViewById(R.id.autoComplete_Cat);
+                }
+            }
+        });
+    }
 
-		if (node != null) {
+    /**
+     * Give all the tag category's from the Tag-XML.
+     * 
+     * @return a string array containing the category's
+     */
+    public String[] getCategoryTags() {
+        // Testarray
+        String[] firstGroupTags = parseTags(Tags.KEY, "");
+        return firstGroupTags;
+    }
 
-			node.getTags().put(autoComplCat.getText().toString(),
-					autoComplVal.getText().toString());
+    /**
+     * Generate the appendant values for the category tag.
+     * 
+     * @param category
+     * @return return a string array with values for the given category
+     */
+    public String[] getValues(String category) {
+        // return the value tags for the selected category tag
+        String[] valueTags = parseTags(Tags.VALUE, category);
+        return valueTags;
+    }
 
-		}
-		final Intent intent = new Intent(this, AddPointActivity.class);
-		intent.putExtra("DataNodeId", node.getId());
-		startActivity(intent);
-		finish();
-	}
+    /**
+     * Save the MetaData to the Node-Meta and go back to the information
+     * activity.
+     * 
+     * @param view
+     */
+    public void saveBtn(View view) {
 
-	/**
-	 * 
-	 * @param view not used
-	 */
-	public void cancelBtn(View view) { // method signature including view is
-										// required
-		final Intent intent = new Intent(this, AddPointActivity.class);
-		intent.putExtra("DataNodeId", node.getId());
-		startActivity(intent);
-	}
+        final AutoCompleteTextView autoComplVal = (AutoCompleteTextView) findViewById(R.id.autoComplete_Value);
+        final AutoCompleteTextView autoComplCat = (AutoCompleteTextView) findViewById(R.id.autoComplete_Cat);
 
-	/**
-	 * This Method parse all the Tag's in the XML-MetaTag files
-	 * 
-	 * @param tagType
-	 * @param parentName
-	 * @return
-	 */
-	private String[] parseTags(Tags tagType, String parentName) {
-		int next;
-		boolean inParent = false;
-		XmlResourceParser parser = this.getResources().getXml(R.xml.tags);
-		ArrayList<String> tagStrings = new ArrayList<String>();
+        if (node != null) {
 
-		try {
-			String tag = "";
-			next = parser.getEventType();
-			while (next != XmlPullParser.END_DOCUMENT) {
-				if (next == XmlPullParser.START_TAG) {
-					tag = parser.getName();
-					switch (tagType) {
-					case KEY: {
-						if (tag.equals("key")) {
-							tagStrings.add(parser.getAttributeValue(null, "v"));
-						}
-						break;
-					}
-					case VALUE: {
-						if ((tag.equals("key"))
-								&& (parser.getAttributeValue(null, "v"))
-										.equals(parentName)) {
-							inParent = true;
-						} else if ((tag.equals("key"))
-								&& !(parser.getAttributeValue(null, "v"))
-										.equals(parentName)) {
-							inParent = false;
-						} else if (inParent && (tag.equals("value"))) {
-							tagStrings.add(parser.getAttributeValue(null, "v"));
-						}
-						break;
-					}
-					case USEFUL: {
-						if ((tag.equals("value"))
-								&& (parser.getAttributeValue(null, "v"))
-										.equals(parentName)) {
-							inParent = true;
-						} else if ((tag.equals("value"))
-								&& !(parser.getAttributeValue(null, "v"))
-										.equals(parentName)) {
-							inParent = false;
-						} else if (inParent && (tag.equals("useful"))) {
-							tagStrings.add(parser.getAttributeValue(null, "v"));
-						}
-						break;
-					}
-					default:
-						break;
-					}
-				}
+            node.getTags().put(autoComplCat.getText().toString(),
+                    autoComplVal.getText().toString());
 
-				next = parser.next();
-			}
+        }
+        final Intent intent = new Intent(this, AddPointActivity.class);
+        intent.putExtra("DataNodeId", node.getId());
+        startActivity(intent);
+        finish();
+    }
 
-		} catch (XmlPullParserException xe) {
-			parser.close();
-			Log.e("PARSE_TAGS", "Couldn't parse tags from xml");
-			return new String[0];
-		} catch (IOException e) {
-			parser.close();
-			Log.e("PARSE_TAGS", "Couldn't parse tags from xml");
-			return new String[0];
-		} finally {
-			parser.close();
-		}
-		String[] tagStringsArray = new String[tagStrings.size()];
-		return tagStrings.toArray(tagStringsArray);
-	}
+    /**
+     * 
+     * @param view
+     *            not used
+     */
+    public void cancelBtn(View view) { // method signature including view is
+                                       // required
+        final Intent intent = new Intent(this, AddPointActivity.class);
+        intent.putExtra("DataNodeId", node.getId());
+        startActivity(intent);
+    }
+
+    /**
+     * This Method parse all the Tag's in the XML-MetaTag files
+     * 
+     * @param tagType
+     * @param parentName
+     * @return a String array with all tags
+     */
+    private String[] parseTags(Tags tagType, String parentName) {
+        int next;
+        boolean inParent = false;
+        XmlResourceParser parser = this.getResources().getXml(R.xml.tags);
+        ArrayList<String> tagStrings = new ArrayList<String>();
+
+        try {
+            String tag = "";
+            next = parser.getEventType();
+            while (next != XmlPullParser.END_DOCUMENT) {
+                if (next == XmlPullParser.START_TAG) {
+                    tag = parser.getName();
+                    switch (tagType) {
+                    case KEY: {
+                        if (tag.equals("key")) {
+                            tagStrings.add(parser.getAttributeValue(null, "v"));
+                        }
+                        break;
+                    }
+                    case VALUE: {
+                        if ((tag.equals("key"))
+                                && (parser.getAttributeValue(null, "v"))
+                                        .equals(parentName)) {
+                            inParent = true;
+                        } else if ((tag.equals("key"))
+                                && !(parser.getAttributeValue(null, "v"))
+                                        .equals(parentName)) {
+                            inParent = false;
+                        } else if (inParent && (tag.equals("value"))) {
+                            tagStrings.add(parser.getAttributeValue(null, "v"));
+                        }
+                        break;
+                    }
+                    case USEFUL: {
+                        if ((tag.equals("value"))
+                                && (parser.getAttributeValue(null, "v"))
+                                        .equals(parentName)) {
+                            inParent = true;
+                        } else if ((tag.equals("value"))
+                                && !(parser.getAttributeValue(null, "v"))
+                                        .equals(parentName)) {
+                            inParent = false;
+                        } else if (inParent && (tag.equals("useful"))) {
+                            tagStrings.add(parser.getAttributeValue(null, "v"));
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                    }
+                }
+
+                next = parser.next();
+            }
+
+        } catch (XmlPullParserException xe) {
+            parser.close();
+            Log.e("PARSE_TAGS", "Couldn't parse tags from xml");
+            return new String[0];
+        } catch (IOException e) {
+            parser.close();
+            Log.e("PARSE_TAGS", "Couldn't parse tags from xml");
+            return new String[0];
+        } finally {
+            parser.close();
+        }
+        String[] tagStringsArray = new String[tagStrings.size()];
+        return tagStrings.toArray(tagStringsArray);
+    }
 
 }
