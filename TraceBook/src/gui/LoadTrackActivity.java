c672b6a4b9ad33e8de603e064b53dbf41885e1ca
js.schrifterz@googlemail.com <js.schrifterz@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-09 09:04:08 +0000

    new Compilesettings, show all tracks when clicking on load track

--- TraceBook/src/gui/LoadTrackActivity.java
+++ TraceBook/src/gui/LoadTrackActivity.java
@@ -1,8 +1,12 @@
 package gui;
 
+import java.util.List;
+
+import core.data.DataStorage;
 import Trace.Book.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * The Class LoadTrackActivity.
@@ -18,5 +22,15 @@ public class LoadTrackActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.loadtrackactivity);
+		
+		DataStorage storage = DataStorage.getInstance();
+		storage.retrieveTrackNames();
+		List<String> list = storage.getAllTracks();
+		StringBuilder sb = new StringBuilder();
+		for(String str : list) {
+		    sb.append(str);
+		    sb.append("\n");
+		}
+		((TextView)findViewById(R.id.track_textview)).setText(sb);
 	}
 }
