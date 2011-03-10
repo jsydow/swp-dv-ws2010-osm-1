3c9ba05f3c57b4fa2ad57df2eccc8aca787c474b
benpicco <benpicco@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-10 12:08:46 +0000

    update MapsForge and add script to get latest svn revision automatically

---
+++ TraceBook/tools/update_lib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TARGET="../mapsforge"
+
+svn checkout http://mapsforge.googlecode.com/svn/trunk/mapsforge $TARGET
+cd $TARGET
+ant clean
+ant mapsforge-map-jar-create
+cd -
+cp $TARGET/dist/mapsforge-map-0.2.1.jar lib/
