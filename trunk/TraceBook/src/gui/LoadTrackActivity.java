c189938c7bc4ca4dd04b2cafddf6b7ef5c07e62d
js.schrifterz@googlemail.com <js.schrifterz@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-10 11:25:39 +0000

    LoadTrackActivity

--- trunk/TraceBook/src/gui/LoadTrackActivity.java
+++ trunk/TraceBook/src/gui/LoadTrackActivity.java
@@ -2,10 +2,13 @@ package gui;
 
 import Trace.Book.R;
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import core.data.DataStorage;
 
 /**
@@ -14,6 +17,31 @@ import core.data.DataStorage;
  * @author greenTraxas
  */
 public class LoadTrackActivity extends ListActivity {
+
+    /**
+     * @author js
+     * 
+     */
+    public static class LoadTrackArrayAdapter extends ArrayAdapter<String> {
+        /**
+         * @param context
+         */
+        LoadTrackArrayAdapter(Context context) {
+            super(context, R.layout.loadtrack_listitem,
+                    R.id.load_track_item_text, DataStorage.getInstance()
+                            .getAllTracks());
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            String trackname = ((TextView) (view
+                    .findViewById(R.id.load_track_item_text))).getText()
+                    .toString();
+            view.findViewById(R.id.load_track_delete_button).setTag(trackname);
+            return view;
+        }
+    }
 
     /*
      * (non-Javadoc)
@@ -24,16 +52,24 @@ public class LoadTrackActivity extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setListAdapter(new ArrayAdapter<String>(this,
-                R.layout.loadtrack_listitem, R.id.load_track_item_text,
-                DataStorage.getInstance().getAllTracks()));
+        setListAdapter(new LoadTrackArrayAdapter(this));
+
     }
 
     /**
+     * @param v
      * 
      */
     public void deleteTrackBtn(View v) {
-        Log.d("BUTTON", "Pressed #########################");
+        if (v == null) {
+            Log.w("BUTTON", "view is null");
+            return;
+        }
+        if (v.getTag() == null) {
+            Log.w("BUTTON", "tag is null");
+            return;
+        }
+        Log.w("BUTTON", (String) v.getTag());
     }
 
 }
