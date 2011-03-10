4facb3fb83f36722dc6ee17136911bd53b7a040e
js.schrifterz@googlemail.com <js.schrifterz@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-10 10:31:00 +0000

    test implementation of loadtrackactivity

--- TraceBook/src/gui/LoadTrackActivity.java
+++ TraceBook/src/gui/LoadTrackActivity.java
@@ -1,22 +1,19 @@
 package gui;
 
-import java.util.List;
-
 import Trace.Book.R;
-import android.app.Activity;
-import android.content.Intent;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 import core.data.DataStorage;
-import core.data.DataTrack;
 
 /**
  * The Class LoadTrackActivity.
  * 
  * @author greenTraxas
  */
-public class LoadTrackActivity extends Activity {
+public class LoadTrackActivity extends ListActivity {
 
     /*
      * (non-Javadoc)
@@ -26,40 +23,17 @@ public class LoadTrackActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loadtrackactivity);
 
-        String lastTrack = "";
-        DataStorage storage = DataStorage.getInstance();
-        storage.retrieveTrackNames();
-        List<String> list = storage.getAllTracks();
-        StringBuilder sb = new StringBuilder();
-        for (String str : list) {
-            sb.append(str);
-            sb.append("\n");
-            lastTrack = str;
-        }
-        sb.append("\n");
-        storage.deserialiseTrack(lastTrack);
-        DataTrack track = storage.getTrack(lastTrack);
-        storage.setCurrentTrack(track);
-
-        sb.append("Last Track is: " + track.getName() + "\n");
-        sb.append("Nodes: " + track.getNodes().size() + "\n");
-        sb.append("Media: " + track.getMedia().size() + "\n");
-        sb.append("Ways: " + track.getWays().size() + "\n");
-
-        ((TextView) findViewById(R.id.track_textview)).setText(sb);
-        storage.setCurrentTrack(track);
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.loadtrack_listitem, R.id.load_track_item_text,
+                DataStorage.getInstance().getAllTracks()));
     }
 
     /**
-     * Just for testing.
      * 
-     * @param view
-     *            ...
      */
-    public void loadLastTrack(View view) {
-        Intent intent = new Intent(this, NewTrackActivity.class);
-        startActivity(intent);
+    public void deleteTrackBtn(View v) {
+        Log.d("BUTTON", "Pressed #########################");
     }
+
 }
