8fb7dd87952f6fc371501d077b1582cd434802f2
SakirSoft <SakirSoft@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-13 20:30:29 +0000

    -fixed warnings

--- trunk/TraceBook/src/gui/adapter/GenericAdapter.java
+++ trunk/TraceBook/src/gui/adapter/GenericAdapter.java
@@ -8,30 +8,64 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
+/**
+ * @author SakirSoft The GenericAdapter class helps us it reduce the number of
+ *         customizable ArrayAdapter classes. You can use it for all of you
+ *         custom ListView layouts. It needs a layoutInflater and a reference to
+ *         a list of GenericAdapterData. Each element in the list describes one
+ *         element in a list view.
+ * 
+ */
 public class GenericAdapter extends ArrayAdapter<GenericAdapterData> {
-	
-	int layoutId;
-	LayoutInflater layoutInflater;
-	List<GenericAdapterData> dataList;
-	
-	
-	public GenericAdapter(Context context, int resource,
-			int textViewResourceId, List<GenericAdapterData> objects,LayoutInflater layInf) {
-		super(context, resource, textViewResourceId, objects);
-		// TODO Auto-generated constructor stub
-		layoutId 		= resource;
-		layoutInflater 	= layInf;
-		dataList		= objects;
-	}
-	
-	 @Override
-     public View getView(int position, View convertView, ViewGroup parent) {
-		 
-		 View genView		 		= layoutInflater.inflate(layoutId, null);
-		 GenericAdapterData	data	= dataList.get(position);
-		 data.fillView(genView);
-		 
-		 return genView;
-	 }
+
+    /**
+     * The layout id where the view elements are stored for on custom item in
+     * the list view
+     */
+    int layoutId;
+
+    /**
+     * A reference to a layoutInflater which will be used to inflate the custom
+     * view for an item in the list view
+     * 
+     */
+    LayoutInflater layoutInflater;
+
+    /**
+     * A reference to a list of GenericAdapterData which will be used to fill
+     * the list
+     * 
+     */
+    List<GenericAdapterData> dataList;
+
+    /**
+     * The Standart constructor for an ArrayAdapter plus a reference to a
+     * LayoutInflater
+     * 
+     * @param context
+     * @param resource
+     * @param textViewResourceId
+     * @param objects
+     * @param layInf
+     */
+    public GenericAdapter(Context context, int resource,
+            int textViewResourceId, List<GenericAdapterData> objects,
+            LayoutInflater layInf) {
+        super(context, resource, textViewResourceId, objects);
+        // TODO Auto-generated constructor stub
+        layoutId = resource;
+        layoutInflater = layInf;
+        dataList = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View genView = layoutInflater.inflate(layoutId, null);
+        GenericAdapterData data = dataList.get(position);
+        data.fillView(genView);
+
+        return genView;
+    }
 
 }
--- trunk/TraceBook/src/gui/adapter/GenericAdapterData.java
+++ trunk/TraceBook/src/gui/adapter/GenericAdapterData.java
@@ -10,21 +10,63 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+/**
+ * @author SakirSoft The GenericAdapterData helps you to define content for
+ *         currently tree types of different views. 1. TextView 2. ImageView 3.
+ *         Button
+ * 
+ *         One instance of a GenericAdapterData represents the date for one item
+ *         in the list view. Where one item can hold one or more views. You can
+ *         associate views with String tags via the GenericItemDesciption class.
+ * 
+ *         This class provides you easy acces to fill your views with the right
+ *         data.
+ * 
+ */
 public class GenericAdapterData {
 
+    /**
+     * @author SakirSoft An enum to define the three different types of view in
+     *         a item
+     */
     enum ItemTypes {
-        ItemType_Text, ItemType_Image, ItemType_Button,
+        /**
+         * TextView
+         */
+        ItemType_Text,
+        /**
+         * ImageView
+         */
+        ItemType_Image,
+
+        /**
+         * Button
+         */
+        ItemType_Button,
     }
 
-    public interface GenericItem {
+    private interface GenericItem {
         void fillItem(View view, int id);
     }
 
+    /**
+     * @author SakirSoft The class representation to handle TextView data
+     * 
+     */
     class TextItem implements GenericItem {
 
+        /**
+         * Reference to a string which will be used to fill a text view
+         */
         String text;
+        /**
+         * Reference to a textView to which this item is connect with
+         */
         TextView textView;
 
+        /**
+         * @param text
+         */
         public TextItem(String text) {
             this.text = text;
         }
@@ -37,16 +79,30 @@ public class GenericAdapterData {
 
         }
 
+        /**
+         * @return returns the current text of the TextView which is connect
+         *         with this item
+         */
         public String getCurrentText() {
             return textView.getText().toString();
         }
 
     }
 
+    /**
+     * @author SakirSoft The class representation to handle ImageView data
+     * 
+     */
     class ImageItem implements GenericItem {
 
+        /**
+         * The resource id to an image
+         */
         int imageId;
 
+        /**
+         * @param imageId
+         */
         public ImageItem(int imageId) {
             this.imageId = imageId;
         }
@@ -60,10 +116,21 @@ public class GenericAdapterData {
 
     }
 
+    /**
+     * @author SakirSoft
+     * 
+     */
     class ButtonItem implements GenericItem {
 
+        /**
+         * Reference to an onClickListener whichh will be called when an onClick
+         * event occurs.
+         */
         View.OnClickListener onClickListener;
 
+        /**
+         * @param onClickListener
+         */
         public ButtonItem(View.OnClickListener onClickListener) {
             this.onClickListener = onClickListener;
         }
@@ -77,13 +144,25 @@ public class GenericAdapterData {
 
     }
 
+    /**
+     * 
+     */
     GenericItemDescription description;
+    /**
+     * 
+     */
     Map<String, GenericItem> items = new HashMap<String, GenericItem>();
 
+    /**
+     * @param desc
+     */
     public GenericAdapterData(GenericItemDescription desc) {
         description = desc;
     }
 
+    /**
+     * @param view
+     */
     public void fillView(View view) {
         Iterator<Entry<String, GenericItem>> iterator = items.entrySet()
                 .iterator();
@@ -96,20 +175,37 @@ public class GenericAdapterData {
 
     }
 
-    public void SetText(String tag, String text) {
+    /**
+     * @param tag
+     * @param text
+     */
+    public void setText(String tag, String text) {
         items.put(tag, new TextItem(text));
     }
 
-    public void SetImage(String tag, int image) {
+    /**
+     * @param tag
+     * @param image
+     */
+    public void setImage(String tag, int image) {
         items.put(tag, new ImageItem(image));
     }
 
-    public void SetButtonCallback(String tag,
+    /**
+     * @param tag
+     * @param onClickListener
+     */
+    public void setButtonCallback(String tag,
             View.OnClickListener onClickListener) {
         items.put(tag, new ButtonItem(onClickListener));
 
     }
 
+    /**
+     * @param tag
+     *            Tag which is associated with the TextView
+     * @return return the text for a given TextView
+     */
     public String getText(String tag) {
         TextItem textItem = (TextItem) items.get(tag);
         return textItem.getCurrentText();
