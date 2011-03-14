7e007ec0285988dfbe9038d1a585b96ee253d58e
js.schrifterz@googlemail.com <js.schrifterz@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-14 12:43:41 +0000

    updated package info

--- trunk/TraceBook/src/core/data/package-info.java
+++ trunk/TraceBook/src/core/data/package-info.java
@@ -1,4 +1,6 @@
 /**
- * 
+ * This package contains the data model of TraceBook. It contains all classes that represent 
+ * the tracking data and all additional meta data that are kept on the devices sd card and in the 
+ * working memory.
  */
 package core.data;
