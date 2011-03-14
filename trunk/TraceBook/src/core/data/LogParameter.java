2e3154ef7b16a887c73bf56c6b64991a50774e08
benpicco <benpicco@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-14 09:13:00 +0000

    fix LogParameter according to checkstyle

--- trunk/TraceBook/src/core/data/LogParameter.java
+++ trunk/TraceBook/src/core/data/LogParameter.java
@@ -8,22 +8,39 @@ import android.os.Parcelable;
  * 
  */
 public class LogParameter implements Parcelable {
-    /**
-	 * 
-	 */
-    public int id = -1;
-    /**
-	 * 
-	 */
-    public int deltaDistance = 0;
-    /**
-	 * 
-	 */
-    public int deltaTime = 0;
+
+    private int id = -1;
+    private int deltaDistance = 0;
+    private int deltaTime = 0;
 
     /**
-	 * 
-	 */
+     * get ID of the new Track object.
+     * 
+     * @return id of the Track object
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return The distance between two positions before a new gps fix is
+     *         generated
+     */
+    public int getDeltaDistance() {
+        return deltaDistance;
+    }
+
+    /**
+     * 
+     * @return the time after which a new gps fix is generated
+     */
+    public int getDeltaTime() {
+        return deltaTime;
+    }
+
+    /**
+     * required for {@link Parcelable}.
+     */
     public static final Parcelable.Creator<LogParameter> CREATOR = new Parcelable.Creator<LogParameter>() {
 
         public LogParameter createFromParcel(Parcel in) {
