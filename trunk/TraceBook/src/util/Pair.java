d9ff105ae3d51813152e8058c408e134d99100ac
benpicco <benpicco@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-15 11:27:25 +0000

    make Pair checkstyle conform

--- trunk/TraceBook/src/util/Pair.java
+++ trunk/TraceBook/src/util/Pair.java
@@ -14,12 +14,52 @@ public class Pair<A, B> {
     /**
      * first value stored in the Pair.
      */
-    public A first;
+    A first;
 
     /**
      * second value stored in the Pair.
      */
-    public B second;
+    B second;
+
+    // I hate you, checkstyle, I hate you.
+
+    /**
+     * Gets the first element.
+     * 
+     * @return the first element
+     */
+    public A getFirst() {
+        return first;
+    }
+
+    /**
+     * set the first element.
+     * 
+     * @param first
+     *            the first element
+     */
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    /**
+     * Gets the second element.
+     * 
+     * @return the second element
+     */
+    public B getSecond() {
+        return second;
+    }
+
+    /**
+     * set the second element.
+     * 
+     * @param second
+     *            the first element
+     */
+    public void setSecond(B second) {
+        this.second = second;
+    }
 
     /**
      * Create a new Pair.
