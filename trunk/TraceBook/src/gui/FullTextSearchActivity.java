e6bb4229ef6baa0f8f4b2fb4ca4f06427db8266f
SakirSoft <SakirSoft@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-15 10:49:27 +0000

    -add textwatcher

--- trunk/TraceBook/src/gui/FullTextSearchActivity.java
+++ trunk/TraceBook/src/gui/FullTextSearchActivity.java
@@ -3,13 +3,53 @@ package gui;
 import Trace.Book.R;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
+/**
+ * 
+ * @author Sahin
+ * 
+ */
 public class FullTextSearchActivity extends ListActivity {
+
+    /**
+     * The text watcher tracks changes in the search edit box. As soon as some
+     * text matches with the description text, the result will be displayed in
+     * the listview.
+     * 
+     * @author sahin
+     * 
+     */
+    class myTextWatcher implements TextWatcher {
+
+        public void afterTextChanged(Editable arg0) {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                int arg3) {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+                int arg3) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_fulltextsearchactivity);
+
+        EditText editBox = (EditText) findViewById(R.id.et_fulltextsearchfullActivity_search);
+        editBox.addTextChangedListener(new myTextWatcher());
 
     }
 }
