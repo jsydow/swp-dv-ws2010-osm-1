e943e2e5c8c8bd56ac92107a0d5db6a4d3b81f80
Skudo84@gmail.com <Skudo84@gmail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-16 13:43:04 +0000

    Modified output and invocation.

--- TraceBook/tools/ldiff.sh
+++ TraceBook/tools/ldiff.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 
-OLDFILE=$1
-NEWFILE=$2
+if [ $# -eq 0 ]
+then
+    echo "USAGE: ldiff.sh LANG"
+    echo "  LANG is the 2-letter language code representation."
+    exit 1
+fi
+
+OLDFILE="res/values/strings.xml"
+NEWFILE="res/values-$1/strings.xml"
+
+if [ ! -r $OLDFILE ]
+then
+    echo $OLDFILE does not exist.
+    exit 2
+fi
+
+if [ ! -r $NEWFILE ]
+then
+    echo $NEWFILE does not exist.
+    exit 3
+fi
 
 OLDTMP=`mktemp`
 NEWTMP=`mktemp`
@@ -9,5 +28,5 @@ NEWTMP=`mktemp`
 grep 'string name="' "$OLDFILE" | sed -e 's/>.*//g' -e 's/.*name=//g' -e 's/"//g' | sort -u > $OLDTMP
 grep 'string name="' "$NEWFILE" | sed -e 's/>.*//g' -e 's/.*name=//g' -e 's/"//g' | sort -u > $NEWTMP
 
-diff -w -U 0 $OLDTMP $NEWTMP
+diff -w -U 0 $OLDTMP $NEWTMP | grep ^\[+-\]
 rm -f $OLDTMP $NEWTMP
