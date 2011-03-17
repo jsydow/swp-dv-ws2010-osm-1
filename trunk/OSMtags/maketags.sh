284745a8c8b8d7fbc3af0c9a6b5c75b5516e6076
Skudo84@gmail.com <Skudo84@gmail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-17 10:19:23 +0000

    Pushing XML files straight to TraceBook.

--- trunk/OSMtags/maketags.sh
+++ trunk/OSMtags/maketags.sh
@@ -3,7 +3,8 @@
 LANG="EN DE FR Tr Pl"
 
 for L in $LANG ; do
-    ./get.rb $L > xml/tags.$L.xml
-    echo Done parsing $L.
+    ./get.rb $L > xml/tags_`echo $L | tr A-Z a-z`.xml
+    echo Done parsing `echo $L`.
 done
 tar cjvf xml.tar.bz2 xml && scp xml.tar.bz2 hdinh@login.zedat.fu-berlin.de:public_html/
+cp xml/tags_*.xml $HOME/development/TraceBook/res/raw/
