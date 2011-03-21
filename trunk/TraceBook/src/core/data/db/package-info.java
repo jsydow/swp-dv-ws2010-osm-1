7c4c6e6a1b108d6cfb3028a5c310d6efa0fac94b
js.schrifterz@googlemail.com <js.schrifterz@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-21 12:44:14 +0000

    issue #40 fixed

--- trunk/TraceBook/src/core/data/db/package-info.java
+++ trunk/TraceBook/src/core/data/db/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Contains.
+ * Contains all database access to the tag database. There are two databases maintained by this package.
+ * The History table stores a history of all used tags (see HistoryDb). The TagDb contains information on all tags.
  */
 package core.data.db;
