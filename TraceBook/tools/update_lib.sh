1a66a1ac3cbd98f47a44cc7f8c69850e386389b2
benpicco <benpicco@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-22 11:22:47 +0000

    update to latest MapsForge API, Fixes issue 76

--- TraceBook/tools/update_lib.sh
+++ TraceBook/tools/update_lib.sh
@@ -4,6 +4,7 @@ TARGET="../mapsforge"
 svn checkout http://mapsforge.googlecode.com/svn/trunk/mapsforge $TARGET
 cd $TARGET
 ant clean
+mapsforge-map-javadoc-create
 ant mapsforge-map-jar-create
 cd -
 cp $TARGET/dist/mapsforge-map-0.2.1.jar lib/
