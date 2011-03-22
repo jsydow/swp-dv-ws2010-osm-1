d6ce8552cd828f554abf3ddd76106e12ded600e6
js.schrifterz@googlemail.com <js.schrifterz@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-22 13:15:49 +0000

    fixed issue #78

--- trunk/TraceBook/src/tracebook/package-info.java
+++ trunk/TraceBook/src/tracebook/package-info.java
@@ -1,4 +1,12 @@
 /**
- * This is the root package for all classes used by TraceBook.
+ * Tracebook is an android application that allows the user to track way 
+ * and point of interests in order to gather information for the 
+ * Open Street Map project. The gathered data can be exported and then 
+ * imported to JOSM.
+ * 
+ * <p>
+ * 
+ * For further information on the internals see the package information of the inner packages.
+ * 
  */
 package tracebook;
