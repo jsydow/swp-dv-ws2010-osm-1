0cebce91c96f41cf7fd1efe9014c6e8c65795b95
js.schrifterz@googlemail.com <js.schrifterz@googlemail.com@5eff381b-bd8e-e3be-69db-190002558077>
2011-03-23 13:38:38 +0000

    deleted some todos

--- TraceBook/src/tracebook/gui/activity/PreferencesActivity.java
+++ TraceBook/src/tracebook/gui/activity/PreferencesActivity.java
@@ -52,7 +52,6 @@ public class PreferencesActivity extends PreferenceActivity {
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
-        // TODO Auto-generated method stub
         super.onConfigurationChanged(newConfig);
     }
 
@@ -86,7 +85,6 @@ public class PreferencesActivity extends PreferenceActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // TODO Auto-generated method stub
         super.onActivityResult(requestCode, resultCode, data);
 
         if (data != null) {
@@ -109,7 +107,6 @@ public class PreferencesActivity extends PreferenceActivity {
 
     @Override
     protected void onResume() {
-        // TODO Auto-generated method stub
         super.onResume();
 
         mapChooser.setSummary(appPreferences.getString("mapsforgeMapFilePath",
